a3f059a95b447a1e0bbb9215af4dac17668ff605
seanicus64@seanpatnaude@gmail.com <seanpatnaude@gmail.com>
2018-03-04 01:37:49 -0500

    cleaned up code

--- tvbet.py
+++ tvbet.py
@@ -11,8 +11,6 @@ subreddit = reddit.subreddit("sje46")
 all_shows = ["Mr_Robot", "Star_Wars", "GOT", "BCS", "US_Politics", "reddit_drama"]
 def check_player(author):
     name = author.name
-    
-#    cur.execute("SELECT * FROM 'bank'")
     cur.execute("SELECT * FROM bank WHERE player = ? ", (name,))
     results = cur.fetchall()
     if not results:
@@ -20,26 +18,25 @@ def check_player(author):
         con.commit()
 
 
-
+def handle_offer(comment):
+    offer, cat_id, end_date, reveal_date, options = parse_offer(comment)
+    bet_id = SQL_add_offer(offer, cat_id, comment.author.name, int(comment.created_utc), \
+        end_date, reveal_date, comment.id, options)
+    labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for e, o in enumerate(options):
+        SQL_add_option(o[0], bet_id, o[1], labels[e])
+    text = reply_offer_bet(bet_id)
+    comment.reply(text)
 def parse_offer(comment):
     # first line must be $add [label] [question]
-    # two things:
-    #   check/add player to the game (bank)
-    #   check if bet already added
-    #TODO: parsing options is way too strict!  Careful
-    # with whitespace, etc
-    print("parsing offer")
     check_player(comment.author)
-    response = ""
     text = comment.body
     lines = text.split("\n")
     lines = [x.strip() for x in lines]
     first_line = lines[0].split()
-    label = first_line[1]
-    question = " ".join(first_line[2:])
+    offer = " ".join(first_line[2:])
 
     options = list(filter(lambda l: l.startswith("* "), lines[1:]))
-    print("options")
     confirmed = []
     for o in options:
         split = o.split()
@@ -47,181 +44,150 @@ def parse_offer(comment):
         try:
             odds = float(odds)
         except ValueError:
-            print("problem with floating({})".format(odds))
             return
         if odds < 1:
             return
-
         option = " ".join(split[2:])
         confirmed.append((option, odds))
-    print(confirmed)
-#        put_in, _, winnings = odds.partition("/")
-#        if len(put_in.split()) == 1 and put_in.isdigit() and \
-#            len(winnings.split()) == 1 and winnings.isdigit():
-#                multiplier = int(winnings) / int(put_in)
-#                confirmed.append(
-#        left, _, right = o.partition(" - ")
-##        left = left.strip("* ")
- #3       put_in, _, winnings = right.partition("/")
-#        if len(put_in.split()) == 1 and put_in.isdigit() and \
-#            len(winnings.split()) == 1 and winnings.isdigit():
-#                multiplier = int(winnings) / int(put_in) 
-#                confirmed.append((left, multiplier))
     for line in lines:
         if line.lower().startswith("end:"):
             end_date = dateparser.parse(line[4:])
         if line.lower().startswith("reveal:"):
             reveal_date = dateparser.parse(line[7:])
-    print("CONFIRMED")
-    print(confirmed)
-    bet_id = add_bet(question, comment.author.name, \
-        int(comment.created_utc), int(end_date.timestamp()), \
-        int(reveal_date.timestamp()), comment.id, confirmed)
-    print("BET ID IS")
-    print(bet_id)
-    if not bet_id:
-        return
-    text = reply_new_bet(bet_id)
-    comment.reply(text)
-def add_option(option_text, bet_id, multiplier, label):
+        if line.lower().startswith("category:"):
+            line = line.lower()
+            category = line.lstrip("category:").strip()
+    cat_id = SQL_get_cat_id(category)
+    response = (offer, cat_id, end_date.timestamp(), reveal_date.timestamp(), confirmed)
+    return response
+def SQL_get_cat_id(category):
     cur.execute("""
-    INSERT INTO options 
-        (option, bet_id, multiplier, label) 
-    VALUES
-        (?, ?, ?, ?)
-    """, (option_text, bet_id, multiplier, label))
-    con.commit()
-def add_bet(bet, author, created, end, reveal, source, options):
+        SELECT 1 FROM categories 
+        WHERE lower(short) = ?
+        """, (category,))
+    response = cur.fetchone()
+    print(category)
+    print(response)
+    if not response: raise Exception
+    return response[0]
+def SQL_add_offer(bet, cat_id, author, created, end, reveal, source, options):
     cur.execute("""
-    SELECT 1 FROM bets 
-    WHERE source = ?""", (source,))
+        SELECT 1 FROM bets 
+        WHERE source = ?
+        """, (source,))
     response = cur.fetchone()
     if response:
         return
     cur.execute("""
-    INSERT INTO bets 
-        (bet, creator, created, expires, 
-        reveal, source, ended) 
-    VALUES
-        (?, ?, ?, ?, ?, ?, ?)
-    """, (bet, author, created, end, reveal, source, 0))
+        INSERT INTO bets 
+            (bet, creator, created, expires, 
+            reveal, source, ended, cat_id) 
+        VALUES
+            (?, ?, ?, ?, ?, ?, ?, ?)
+    """, (bet, author, created, end, reveal, source, 0, cat_id))
     cur.execute("""SELECT bet_id FROM bets WHERE source=?""", \
             (source,))
     bet_id = cur.fetchone()[0]
-    labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    for e, o in enumerate(options):
-        add_option(o[0], bet_id, o[1], labels[e])
     con.commit()
     return bet_id
-def find_option_taken(bet_id, label):
+def SQL_add_option(option_text, bet_id, multiplier, label):
     cur.execute("""
-    SELECT option_id FROM options
-    WHERE bet_id = ? AND label = ?""", (bet_id, label))
-    result = cur.fetchone()
-    return result
-def take_bet(name, option_id, amount, source):
-    cur.execute("""
-    INSERT INTO amounts
-        (option_id, bettor, amount, source)
-        VALUES (?, ?, ?, ?)
-        """, (option_id, name, amount, source))
+        INSERT INTO options 
+            (option, bet_id, multiplier, label) 
+        VALUES
+            (?, ?, ?, ?)
+        """, (option_text, bet_id, multiplier, label))
     con.commit()
-    
-def check_if_enough_money(name, amount):
-    cur.execute("""
-    SELECT balance FROM bank
-    WHERE player = ?
-    """, (name,))
-    result = cur.fetchone()
-    result = int(result[0])
-    if result < amount:
-        return False
-    return True
-def parse_take_bet(comment):
+def reply_offer_bet(bet_id):
+    bet, creator, created, expires, reveal, ended, cat_id = \
+        SQL_bet_info(bet_id)[0]
+    options = SQL_option_info(bet_id)
+    text = ""
+    text += "--------  \n\n"
+    text += "[{}:{}] **Bet**: {}  \n\n\n".format(cat_id, bet_id, bet)
+    text += "|Label|Option|Odds|Probability|\n"
+    text += "|----:|:-----|:--:|:---------:|\n"
+    for o in SQL_option_info(bet_id):
+        _, option, _, multiplier, label = o
+        probability = 1/multiplier
+        text += "|**{}**|{}|{}|{:.3f}|\n".format(label, option, multiplier, probability)
+    return text
+def handle_bet(comment):
+    option_id, amount = parse_bet(comment)
+    name = comment.author.name
     source = comment.id
+    check_player(comment.author)
+    if not check_if_enough_money(name, amount):
+        return
+    SQL_take_bet(name, option_id, amount, source)
+
+def parse_bet(comment):
     lines = comment.body.split("\n")
     first_line = lines[0].split()
     _, bet_id, label, amount = first_line[:4]
     amount = int(amount)
     name = comment.author.name
-    check_player(comment.author)
-    option_id = find_option_taken(bet_id, label)
+    option_id = find_option(bet_id, label)
     if not option_id: 
-        print("invalid option")
         return
     option_id = option_id[0]
-    if not check_if_enough_money(name, amount):
-        print("not enough money")
-        return
-    take_bet(name, option_id, amount, source)
-#    SQL_bet_info(bet_id)
-def SQL_bet_info(bet_id):
-    print(bet_id)
+    return option_id, amount
+def SQL_find_option(bet_id, label):
     cur.execute("""
-    SELECT bet, creator, created, expires, reveal, ended
-    FROM bets WHERE bet_id = ?
-    """, (bet_id,))
+        SELECT option_id FROM options
+        WHERE bet_id = ? AND label = ?
+        """, (bet_id, label))
+    result = cur.fetchone()
+    return result
+def SQL_take_bet(name, option_id, amount, source):
+    cur.execute("""
+        INSERT INTO amounts
+            (option_id, bettor, amount, source)
+            VALUES (?, ?, ?, ?)
+            """, (option_id, name, amount, source))
+    con.commit()
+    
+def check_if_enough_money(name, amount):
+    cur.execute("""
+        SELECT balance FROM bank
+        WHERE player = ?
+        """, (name,))
+    result = cur.fetchone()
+    result = int(result[0])
+    if result < amount:
+        return False
+    return True
+def SQL_bet_info(bet_id):
+    cur.execute("""
+        SELECT bet, creator, created, expires, reveal, ended, cat_id
+        FROM bets WHERE bet_id = ?
+        """, (bet_id,))
     result = cur.fetchall()
 
-    print(result)
     return result
 def SQL_option_info(bet_id):
     cur.execute("""
-    SELECT * FROM options WHERE bet_id = ?
-    """, (bet_id,))
+        SELECT * FROM options WHERE bet_id = ?
+        """, (bet_id,))
     result = cur.fetchall()
     return result
-def reply_new_bet(bet_id):
-    import fractions
-    print("++++++++++++")
-    print(bet_id)
-    bet, creator, created, expires, reveal, ended = \
-        SQL_bet_info(bet_id)[0]
-    options = SQL_option_info(bet_id)
-    text = ""
-    text += "--------  \n\n"
-    text += "[{}] **Bet**: {}  \n\n\n".format(bet_id, bet)
-    text += "|Label|Option|Odds|Multiplier|\n"
-    text += "|:----|:-----|:--:|:--------:|\n"
-    for o in SQL_option_info(bet_id):
-        _, option, _, multiplier, label = o
-        fraction = str(fractions.Fraction(multiplier).limit_denominator(1000))
-        if "/" not in fraction:
-            fraction += "/1"
-        text += "|**{}**|{}|{}|{}|\n".format(label, option, fraction, multiplier)
+def handle_call(comment):
+    pass
+def parse_call(comment):
+    # !call_bet 123 A
+    pass
     
-    print(text)
-    return text
-
-
-    
-    #    lines.append(str(
-     
-
-    
-
-
 submission = reddit.submission(id="80a8c9")
 submission.comment_sort = "old"
 for comment in submission.comments.list():
     text = comment.body.split()
-    replied_already = False
-    for c in comment.replies:
-        if c.author == reddit.redditor("tvbetbot"):
-            replied_already = True
-    #if replied_already:
-    #    continue
     if len(text) >= 2:
-        if text[0] == "!add":
-#            try:
-                parse_offer(comment)
-#                parse_take_bet(comment)
-#            except:
-#                print("bad syntax")
-#        if text[0] == "bet" and text[1].isdigit():
-#            body = """
-#{} bet {}!\n-----
-#current amount: {}""".format(comment.author, text[1], 54000)
-#            comment.reply(body)
-#        if text[0] == "!add":
-#            parse_bet(comment)
+        if text[0] == "!offer_bet":
+            try: handle_offer(comment)
+            except: pass
+        if text[0] == "!bet":
+            pass
+        if text[0] == "!call_bet":
+            handle_call(comment)
+
+            
